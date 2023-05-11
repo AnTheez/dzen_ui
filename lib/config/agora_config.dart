f26938b9fbd5502907f1c7b871b213426973b979
Aiter <vsimvs.17879@gmail.com>
2023-05-11 13:03:21 +0300

    Add large files

---
+++ lib/config/agora_config.dart
@@ -0,0 +1,5 @@
+class AgoraConfig {
+  static String token = '';
+  static String appId = '7d3df7fc34254fa688a4432535c95aae';
+  static String appcertificate = '28f692984c334de0ad20d71e1b48315a';
+}
